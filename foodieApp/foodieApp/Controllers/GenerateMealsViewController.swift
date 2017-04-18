cf6c399439528c874c9a24e67cd80802ffb14242
dmlcn6 <dmlcn6@mail.missouri.edu>
2017-04-18 04:17:14 -0500

    Scrapped the tableView, COLLECTIONVIEWS FTW!

--- foodieApp/foodieApp/Controllers/GenerateMealsViewController.swift
+++ foodieApp/foodieApp/Controllers/GenerateMealsViewController.swift
@@ -8,22 +8,31 @@
 
 import UIKit
 
-class GenerateMealsViewController: UIViewController {
+class GenerateMealsViewController: UIViewController{
     
     var spoonApiAccess = SpoonApi()
     
-    @IBOutlet weak var mealsTableView: UITableView!
     @IBOutlet weak var dietInput: UITextField!
     @IBOutlet weak var excludedInput: UITextField!
     @IBOutlet weak var caloriesInput: UITextField!
+    @IBOutlet weak var mealsCollectionView: UICollectionView!
     
+    var breakfast = [FoodieRecipe]()
+    var lunch = [FoodieRecipe]()
+    var dinner = [FoodieRecipe]()
     var meals = [FoodieRecipe]()
     var mealErrors = [String]()
+    let indicator = ActivityIndicatorController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        // Header pin Is only feature of UICollectionViewFlowLayout not UICollectionViewLayout
+        if let layout = mealsCollectionView.collectionViewLayout as? UICollectionViewFlowLayout{
+            layout.sectionHeadersPinToVisibleBounds = true
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,56 +40,7 @@ class GenerateMealsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    // MARK: - TableView Config
-    func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 1
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        var rowCount = 0
-        
-        if meals.count > 0{
-            rowCount = meals.count
-        }else if(mealErrors.count > 0){
-            rowCount = mealErrors.count
-            
-        }
-        return rowCount
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        //loads only once\
-        //let selectedRecipeId = meals[indexPath.row].recipeId
-        
-    }
-    
-    // Configure the cell \\
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        //get resuable cell name
-        let cell = tableView.dequeueReusableCell(withIdentifier: "mealPlan", for: indexPath) as! MealTableViewCell
-        
-        if (meals.count > 0) {
-            cell.isUserInteractionEnabled = true
-            
-            //set recipeImageView
-            cell.mealImageView.image = meals[indexPath.row].recipeImage
-            
-            //set recipeLabel
-            cell.mealLable.text = meals[indexPath.row].recipeName
-            
-        }else if (mealErrors.count > 0){
-            cell.mealLable.text = mealErrors[indexPath.row]
-            
-            cell.mealImageView.image = UIImage()
-            
-            cell.isUserInteractionEnabled = false
-        }
-        return cell
-    }
-    
+    // MARK: - SpoonApi
     @IBAction func generateMealPlan(_ sender: Any) {
         meals.removeAll()
         mealErrors.removeAll()
@@ -142,16 +102,23 @@ class GenerateMealsViewController: UIViewController {
         
         //var queryString = "diet=\(diet)&exclude=\(exclude)&targetCalories=\(calories)&timeFrame=week"
         
+        indicator.setup(parentView: self.view)
+        indicator.start()
+        
         //loads only once\\
         spoonApiAccess.getMealPlanData(queryString: queryString){
             (data,error) in
             
             if let data = data {
                 self.parseMealPlanJson(data: data)
-                self.mealsTableView.reloadData()
+                self.mealsCollectionView.reloadData()
+                
+                self.indicator.stop()
             }else if let error = error{
                 self.mealErrors.append(error.description)
-                self.mealsTableView.reloadData()
+                self.mealsCollectionView.reloadData()
+                
+                self.indicator.stop()
             }
         }
     }
@@ -190,7 +157,7 @@ class GenerateMealsViewController: UIViewController {
                 if (key == "items"){
                     if let resultsArray = value as? [[String:Any]] {
                         //print("\n\nRESULTSARRAY\(resultsArray)\n\n")
-                        
+                        var foodHour = 0
                         for result in resultsArray {
                             //print("\n\nRESULTSPART\(result)\n\n")
                             for (key, value) in result{
@@ -200,7 +167,9 @@ class GenerateMealsViewController: UIViewController {
                                     
                                 }
                                 if (key == "slot"){
-                                    
+                                    if let value = value as? Int{
+                                        foodHour = value
+                                    }
                                 }
                                 if let value = value as? String, key == "value" {
                                     if let dataValue = value.data(using: .utf8){
@@ -243,7 +212,7 @@ class GenerateMealsViewController: UIViewController {
                                                     }
                                                 }
                                             }
-                                            
+                                            savingRecipe.foodHour = foodHour
                                             savingRecipe.recipeServings = 0
                                             savingRecipe.recipeTime = 0
                                             
@@ -254,6 +223,25 @@ class GenerateMealsViewController: UIViewController {
                             }
                         }
                     }
+                }
+            }
+            
+            sortMeals(recipeArray: meals)
+        }
+    }
+    
+    func sortMeals(recipeArray: [FoodieRecipe]){
+        for meal in meals{
+            if let foodHour = meal.foodHour{
+                switch foodHour {
+                case 1:
+                    breakfast.append(meal)
+                case 2:
+                    lunch.append(meal)
+                case 3:
+                    dinner.append(meal)
+                default:
+                    break
                 }
             }
         }
@@ -268,5 +256,120 @@ class GenerateMealsViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    func hexStringToUIColor (hex:String) -> UIColor {
+        var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+        
+        if (cString.hasPrefix("#")) {
+            cString.remove(at: cString.startIndex)
+        }
+        
+        if ((cString.characters.count) != 6) {
+            return UIColor.gray
+        }
+        
+        var rgbValue:UInt32 = 0
+        Scanner(string: cString).scanHexInt32(&rgbValue)
+        
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
 
 }
+extension GenerateMealsViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "mealPlan", for: indexPath) as! MealCollectionViewCell
+        
+        if (meals.count > 0){
+            switch indexPath.section {
+            case 0:
+                cell.mealImageView.image = breakfast[indexPath.row].recipeImage
+                cell.mealLabel.text = breakfast[indexPath.row].recipeName
+            case 1:
+                cell.mealImageView.image = lunch[indexPath.row].recipeImage
+                cell.mealLabel.text = lunch[indexPath.row].recipeName
+            case 2:
+                cell.mealImageView.image = dinner[indexPath.row].recipeImage
+                cell.mealLabel.text = dinner[indexPath.row].recipeName
+            default:
+                break
+            }
+            
+        }
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        
+        let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "foodHour", for: indexPath) as! MealHeaderReusableView
+        
+        if (meals.count > 0){
+            switch kind {
+            case UICollectionElementKindSectionHeader:
+                var mealHeader = ""
+                var mealHeaderColor = UIColor()
+
+                if (indexPath.section == 0) {
+                    mealHeader = "Breakfast"
+                    mealHeaderColor = hexStringToUIColor(hex: "#ffbd04")
+                }else if (indexPath.section == 1) {
+                    mealHeader = "Lunch"
+                    mealHeaderColor = hexStringToUIColor(hex: "#eb8c00")
+                }else {
+                    mealHeader = "Main Course"
+                    mealHeaderColor = hexStringToUIColor(hex: "#dc6900")
+                }
+                
+                header.backgroundColor = mealHeaderColor
+                header.mealHeader.text = mealHeader
+                
+                
+            default:
+                assert(false, "Unexpected element kind")
+            }
+        }
+        
+        return header
+    }
+    
+    
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        var count = 0
+        if (meals.count > 0){
+            
+            switch section{
+                case 0:
+                    count = breakfast.count
+                case 1:
+                    count = lunch.count
+                case 2:
+                    count = dinner.count
+                default:
+                    return 0
+            }
+            
+            return count
+            
+        }else{
+            return 0
+        }
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
+}
+
+
+
