9a4f60bd5068bf20ed1a46d1c8fd4e8177fa9b16
dmlcn6 <dmlcn6@mail.missouri.edu>
2017-04-18 04:11:31 -0500

    Added supplementary view for meal plans

---
+++ foodieApp/foodieApp/ViewCells/MealHeaderReusableView.swift
@@ -0,0 +1,14 @@
+//
+//  MealHeaderReusableView.swift
+//  foodieApp
+//
+//  Created by Mr. Lopez on 4/9/17.
+//  Copyright © 2017 DLopezPrograms. All rights reserved.
+//
+
+import UIKit
+
+class MealHeaderReusableView: UICollectionReusableView {
+    @IBOutlet weak var mealHeader: UILabel!
+        
+}
