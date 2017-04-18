5cb56c5c53bba73ea957fb5b4e0d6d2cde3cafb2
dmlcn6 <dmlcn6@mail.missouri.edu>
2017-04-18 04:12:05 -0500

    Added cell for collection view

---
+++ foodieApp/foodieApp/ViewCells/MealCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MealCollectionViewCell.swift
+//  foodieApp
+//
+//  Created by Mr. Lopez on 4/9/17.
+//  Copyright © 2017 DLopezPrograms. All rights reserved.
+//
+
+import UIKit
+
+class MealCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var mealImageView: UIImageView!
+    @IBOutlet weak var mealLabel: UILabel!
+    
+    
+}
